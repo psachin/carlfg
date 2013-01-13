46b756b69d74a62c2c8e57ce4db1fc10761febe2
psachin <iclcoolster@gmail.com>
2013-01-13 17:46:01 +0530

    ready to use

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(name='carlfg',
       version='0.2',
-      description='find CG coefficients,find the sum of all natural numbers from 1 to n',
+      description='find CG coefficients, find the sum of all natural numbers from 1 to n',
       url='https://github.com/psachin/carlfg',
       author='sachin',
       author_email='isachin@iitb.ac.in',
-      license='LICENSE.rst',
+      license=open('LICENSE.rst').read(),
       packages=['cg'],
       scripts=['bin/cg','bin/clebg'],
-      long_description=open('README').read(),
+      long_description=open('README.rst').read(),
+      keywords=['cg','clebg','pypi','coefficients','clebsch gordan'],
 #      install_requires=["python >= 2.7.3",
 #                        ],
       zip_safe=False)
