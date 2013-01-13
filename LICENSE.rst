46583cce8196445f734624f3e551e9fdb6ccf41f
psachin <iclcoolster@gmail.com>
2013-01-13 16:27:45 +0530

    v0.2: frontend to CG coefficients

--- LICENSE.rst
+++ LICENSE.rst
@@ -1,1 +1,1 @@
-GNU GPLv3
+`GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>`_
--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(name='carlfg',
-      version='0.1',
-      description='find the sum of all natural numbers from 1 to n',
+      version='0.2',
+      description='find CG coefficients,find the sum of all natural numbers from 1 to n',
       url='https://github.com/psachin/carlfg',
       author='sachin',
       author_email='isachin@iitb.ac.in',
       license='LICENSE.rst',
       packages=['cg'],
-      scripts=['bin/cg'],
-      long_description=open('README.txt').read(),
+      scripts=['bin/cg','bin/clebg'],
+      long_description=open('README').read(),
 #      install_requires=["python >= 2.7.3",
 #                        ],
       zip_safe=False)
